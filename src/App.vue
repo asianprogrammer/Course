2ebbd52f98971a5dfc8ce42cc5dd4c0bef5187ca
It's Me <itsme@sorry.com>
2023-07-09 12:52:31 +0600

    Project Setup Done

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,3 @@
-<template></template>
+<template>
+    <h1 class="cursor-point">Hello World</h1>
+</template>
---
+++ src/components/NaveBar.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav>
+        <section class="logo">
+            <div class="logo-image">
+                <img :width="'100%'" src="../assets/image/Course.png" alt="Course">
+            </div>
+        </section>
+    </nav>
+</template>
