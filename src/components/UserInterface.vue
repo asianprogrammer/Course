b87bb097cd73563534bb871a6cc83f76a96f41a4
It's Me <itsme@sorry.com>
2023-07-10 09:43:06 +0600

    UI - User Interface Created

---
+++ src/components/UserInterface.vue
@@ -0,0 +1,8 @@
+<script setup>
+import UserBasic from './UserBasic.vue';
+import LearningProgress from './LearningProgress.vue';
+</script>
+<template>
+<UserBasic />
+<LearningProgress />
+</template>
