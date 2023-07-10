e95526e41e1fdc0387fad2cc8579663545472fd4
It's Me <itsme@sorry.com>
2023-07-10 09:44:50 +0600

    WebDev icon created

---
+++ src/components/icons/WebDev.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-code"
+  >
+    <polyline stroke="#0067FF" points="16 18 22 12 16 6"></polyline>
+    <polyline stroke="#0067FF" points="8 6 2 12 8 18"></polyline>
+  </svg>
+</template>
