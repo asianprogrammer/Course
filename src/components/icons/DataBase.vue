a57eed265a59964babaa0597aff1ac09e329527e
It's Me <itsme@sorry.com>
2023-07-10 09:43:45 +0600

    DataBase Icon Created

---
+++ src/components/icons/DataBase.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-database"
+  >
+    <ellipse stroke="#CE76FE" cx="12" cy="5" rx="9" ry="3"></ellipse>
+    <path stroke="#CE76FE" d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path>
+    <path stroke="#CE76FE" d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path>
+  </svg>
+</template>
