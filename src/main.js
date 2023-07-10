697af3e7a01895c6f24fbe29ac97573d3f0dce6b
It's Me <itsme@sorry.com>
2023-07-10 07:19:16 +0600

    Router & Pinia store created

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import './assets/css/main.css'
 import { createApp } from 'vue'
-
+import router from './router/index.js';
+import {createPinia} from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
+app.use(createPinia())
+app.use(router)
 app.mount('#app')
