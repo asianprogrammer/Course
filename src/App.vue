63085d8523d41e23bab9e4661d716590d78cc2f7
It's Me <itsme@sorry.com>
2023-07-10 09:35:47 +0600

    Router Removed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import NavBar from "./components/NaveBar.vue";
+import UserInterface from "./components/UserInterface.vue";
 import Performance from "./components/Performance.vue";
 </script>
 <template>
     <section>
         <NavBar />
         <Performance />
+        <UserInterface />
     </section>
 </template>
