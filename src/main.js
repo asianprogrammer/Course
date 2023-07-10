54c7887781d3c93d0cd467292031a6e38d3c33e6
It's Me <itsme@sorry.com>
2023-07-10 09:38:47 +0600

    Router Removed

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import './assets/css/main.css'
 import { createApp } from 'vue'
-import router from './router/index.js';
 import {createPinia} from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
 app.use(createPinia())
-app.use(router)
 app.mount('#app')
