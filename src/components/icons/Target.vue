da07d35aa88cca23174277a5fa23c4f6ab524811
It's Me <itsme@sorry.com>
2023-07-10 09:44:34 +0600

    Target Icon Created

---
+++ src/components/icons/Target.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-target"
+  >
+    <circle stroke="#FB5456" cx="12" cy="12" r="10"></circle>
+    <circle stroke="#FB5456" cx="12" cy="12" r="6"></circle>
+    <circle stroke="#FB5456" cx="12" cy="12" r="2"></circle>
+  </svg>
+</template>
